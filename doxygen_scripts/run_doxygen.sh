9a40743779f7cf0e54b4ad0490cb8fe0f28fe329
Christopher Backhouse <bckhouse@fnal.gov>
2022-03-01 18:15:59 +0000

    Clear out doxygen directory beforehand, don't error out on latex problems.

--- doxygen_scripts/run_doxygen.sh
+++ doxygen_scripts/run_doxygen.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-time doxygen Doxyfile
+rm -r ../doxygen || exit 1
+mkdir ../doxygen || exit 1
+# Don't connect a terminal so latex can't ask any questions
+time doxygen Doxyfile < /dev/null || exit 1
