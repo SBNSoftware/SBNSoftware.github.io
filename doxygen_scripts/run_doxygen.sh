248d62e3a9c6c52602693ddcc7f46c92fa13d49c
Christopher Backhouse <bckhouse@fnal.gov>
2022-03-18 13:38:04 +0000

    Add infrastructure to automatically merge the doxygen branch.

--- doxygen_scripts/run_doxygen.sh
+++ doxygen_scripts/run_doxygen.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Try and make sure we don't create a big git mess, by starting in the right
+# place and being up to date
+#
+# Make sure we are on the right branch
+[ `git symbolic-ref --short HEAD` == doxygen ] || exit 1
+git pull || exit 1
+
+
 rm -r ../doxygen || exit 1
 mkdir ../doxygen || exit 1
 # Don't connect a terminal so latex can't ask any questions
